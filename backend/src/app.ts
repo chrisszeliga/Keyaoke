1b2f516d03268d03521f9face8fd5e747cf87c94
Chris Szeliga <christopherszeliga@gmail.com>
2024-11-14 20:12:30 -0600

    Fly.io Deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { serveStatic } from 'hono/bun'
 
 import { spotifyRoutes } from './routes/spotifyRoutes';
 
@@ -8,5 +9,8 @@ const app = new Hono()
 app.use(logger())
 
 app.route('/api/spotify', spotifyRoutes)
+
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
